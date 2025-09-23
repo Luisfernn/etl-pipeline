9de8cce0e90a0378d4e0e4d398918f3440829204
Luis Fernando <156948925+Luisfernn@users.noreply.github.com>
2025-09-23 14:00:29 -0300

    Initial commit
    Upload do projeto amd_pipiline

---
+++ amd_pipeline_github/main.py
@@ -0,0 +1,34 @@
+import logging
+from extract import extract_data
+from transform_test import transform_data
+from load import load_data
+
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    
+    ticker = "AMD"
+    logging.info(f"[INFO] Iniciando pipeline para {ticker}")
+
+    history, info, dividends = extract_data(ticker)
+    logging.info("Extração Concluída.")
+
+    historico_df, info_df, dividendos_df = transform_data(history, info, dividends)
+    logging.info("Tranformação Concluída.")
+
+    load_data(historico_df, info_df, dividendos_df, output_dir="data")
+    logging.info("Dados salvos com sucesso na pasta 'data/'.")
+
+    logging.info("Prévia dos dados extraídos e transformados:")
+    print("\nHistórico de preços:")
+    print(historico_df.head(), "\n")
+
+    print("Info (metados):")
+    print(info_df.head(), "\n")
+
+    print("Dividendos:")
+    print(dividendos_df.head(), "\n")
+
+
+if __name__ == "__main__":
+    main()
