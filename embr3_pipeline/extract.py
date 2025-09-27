b20185de5fcb4fb64a10ef4c04487ff682ddba78
Luis Fernando <luisfernandowubi@gmail.com>
2025-09-27 15:19:14 -0300

    Created extract script

---
+++ embr3_pipeline/extract.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+
+def extract_data(ticker='EMBR3'):
+     tk = yf.Ticker(ticker)
+    
+     historico = tk.history(period='6mo')
+     informações = tk.info
+     dividendos = tk.dividends
+
+     return historico, informações, dividendos
