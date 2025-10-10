b1b10e253fc8d8a45c0e1ead429e305499633966
Luis Fernando <luisfernandowubi@gmail.com>
2025-10-10 08:45:26 -0300

    Criando o script de extract

---
+++ adbe_pipeline/extract.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+
+def extract_data(ticker="ADBE"):
+    tk = yf.Ticker(ticker)
+
+    historico = tk.history(period="1y")
+    info = tk.info
+    dividendos = tk.dividends
+
+    return historico, info, dividendos
+
