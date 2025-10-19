280e95837b13c4c60c8e8095b817a171e01c7f4a
Luis Fernando <luisfernandowubi@gmail.com>
2025-10-19 13:02:30 -0300

    Criando o script de load

---
+++ tcehy_pipeline/load.py
@@ -0,0 +1,12 @@
+def load_data(dfs, output_dir="data"):
+    import os 
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    output_path = os.path.join(base_dir, output_dir)
+
+    os.makedirs(output_path, exist_ok=True)
+
+    for name, df in dfs.items():
+        df.to_csv(os.path.join(output_path, f"{name}.csv"), index=False)
+
+    print(f"Arquivos salvos em:{output_path}") 
