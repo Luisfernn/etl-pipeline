48ef95da981c495861e20b4e87315d0019f6a37d
Luis Fernando <luisfernandowubi@gmail.com>
2025-10-19 13:45:16 -0300

    Criando o script de transform

---
+++ aapl_pipeline/transform.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def transform_data(history, info, dividends):
+
+    historico_df = history[["Close", "Volume"]].copy()
+
+    info_df = pd.DataFrame(list(info.items()), columns=["Chave", "Valor"])
+
+    if dividends.empty:
+        dividendos_df = pd.DataFrame([["N/A"]], columns=["Dividendos"])
+    else:
+        dividendos_df = dividends.reset_index()
+        dividendos_df.columns = ["Data", "Dividendo"]   
+
+    return historico_df, info_df, dividendos_df
